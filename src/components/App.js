55ebb87ffa00398612686805bf5bf74069d8ad11
Samarth Srivastava <samarthsrivastava009@gmail.com>
2020-06-24 23:45:20 +0530

    Generalised the notes, with auto render on alter of notes list

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,20 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes";
+
+function createNote(noteItem){
+ return  <Note title={noteItem.title} content={noteItem.content}></Note>
+}
+
+
 
 function App() {
   return (
     <div>
       <Header />
-      <Note />
+      {/* <Note title="this is the title" content="this is the content" /> */}
+      {notes.map(createNote)}
       <Footer />
     </div>
   );
--- src/components/Note.js
+++ src/components/Note.js
@@ -1,10 +1,16 @@
 import React from "react";
+import notes from "../notes.js";
 
-function Note() {
+// function create(val){
+// <h1>{val.title}</h1>
+//   <p>{val.content}</p>
+// }
+
+function Note(props) {
   return (
     <div className="note">
-      <h1>This is the note title</h1>
-      <p>This is the note content</p>
+      <h1>{props.title}</h1>
+      <p>{props.content}</p>
     </div>
   );
 }
